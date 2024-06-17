29de496c754ba89eed01a5a95177120bf41f6040
Mike Schiessl <mschiess@akamai.com>
2024-06-17 13:38:15 +0200

    finalizing v1.7.4

--- bin/uls_config/global_config.py
+++ bin/uls_config/global_config.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Common global variables / constants
-__version__ = "1.7.4-alpha"
+__version__ = "1.7.4"
 __tool_name_long__ = "Akamai Unified Log Streamer"
 __tool_name_short__ = "ULS"
 
